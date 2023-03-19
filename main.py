b5a67d89147b7f51c5c504c65bdbb11147887172
Bala Ayyala <122984990+bayyala@users.noreply.github.com>
2023-03-19 14:06:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Total 50 = while loop
+
+n=int(input("Enter a Number: "))
+total=0
+
+while total<50:# when the total is less than 50, ask the user to iput a number
+   
+   n=int(input("enter a number: "))
+   total=total+n #Adding the another number and total, stop the loopwhen the total is over 50 
+  
+print("the Total =", [total])#the loop is finished")
